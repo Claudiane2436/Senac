1d47325c1d72aae4d920ab3def08de0dd160d6d0
Claudiane <101910551+Claudiane2436@users.noreply.github.com>
2022-12-02 23:42:07 -0300

    Add files via upload

---
+++ Lógica de Programação C#/Atividade 8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal peso;
+            Console.Write("Informe o peso do carro em quilo: " );
+            peso = decimal.Parse (Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (peso<=1000)
+            {
+                Console.WriteLine("Peso adequado pode passar");
+                
+            }
+
+            else
+            {
+                Console.WriteLine("Peso incorreto não pode passar");
+
+            }
+           
+            Console.WriteLine();
+            Console.WriteLine("Aperte qualquer tecla para fechar");
+            Console.ReadKey();
+        }
+    }
+}
