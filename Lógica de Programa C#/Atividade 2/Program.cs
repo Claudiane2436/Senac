e3c485f524251a911fbe4b86c11337f1a5a9be03
Claudiane <101910551+Claudiane2436@users.noreply.github.com>
2022-12-02 22:38:17 -0300

    Nome, idade, cpf e telefone

---
+++ Lógica de Programa C#/Atividade 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome;
+            int idade;
+            string cpf;
+            string telefone;
+
+            Console.Write("Informe o seu nome: ");
+            nome = Console.ReadLine();
+            Console.Write("Informe a sua idade: ");
+            idade= int.Parse(Console.ReadLine());
+            Console.Write ("Informe o seu cpf: ");
+            cpf = Console.ReadLine();
+            Console.Write("Informe o seu telefone: ");
+            telefone = Console.ReadLine();
+
+            Console.WriteLine("Seu nome é: " + nome);
+            Console.WriteLine("Sua idade é: " + idade);
+            Console.WriteLine("O número do seu cpf é: " + cpf);
+            Console.WriteLine("O número do seu tefelone é: " + telefone);
+
+
+            Console.ReadKey();
+        }
+    }
+}
