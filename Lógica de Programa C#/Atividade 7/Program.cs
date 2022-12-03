a929cbbbd515cd3016b1ac77f3bc69a4c2d59986
Claudiane <101910551+Claudiane2436@users.noreply.github.com>
2022-12-02 23:02:52 -0300

    IMC

---
+++ Lógica de Programa C#/Atividade 7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal alt, peso, imc;
+            Console.Write("Digite o seu peso: ");
+            peso = decimal.Parse(Console.ReadLine());
+            Console.Write("Digite a sua altura: ");
+            alt = decimal.Parse(Console.ReadLine());
+
+            imc = peso / (alt * alt);
+            Console.WriteLine("O seu IMC é de " + imc);
+            Console.WriteLine("Aperte qualquer tecla para fechar");
+            Console.ReadKey();
+
+
+
+         
+        }
+    }
+}
